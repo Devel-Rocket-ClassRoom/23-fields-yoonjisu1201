b3dc63e01b4d2e4ce311d403c8fcfa09399b9b50
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-02-24 15:45:29 +0900

    실습 제출합니다.

---
+++ ScoreTracker/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class ScoreTracker
+{
+    public const int maxScore = 100;
+    public const int minScore = 0;
+    public readonly string subjectName;
+    private int score = 0;
+    private int bounsCount = 0;
+
+    public ScoreTracker(string name)
+    {
+        subjectName = name;
+    }
+
+    public void SetScore(int score)
+    {
+        if (score < 0 || score > 100)
+        {
+            Console.WriteLine("점수는 0~100 사이여야 합니다.");
+        }
+        else
+        {
+            Console.WriteLine($"점수를 {score}점으로 설정했습니다.");
+            this.score = score;
+        }
+    }
+
+    public void AddBonus(int bonus)
+    {
+        score += bonus;
+        bounsCount++;
+        if (score > maxScore)
+        {
+            score = maxScore;
+            Console.WriteLine($"{bonus}점 보너스 적용! 현재 점수: {score}점 (최대 점수 도달)");
+        }
+        else
+        {
+            Console.WriteLine($"{bonus}점 보너스 적용! 현재 점수: {score}점");
+
+        }
+    }
+
+    public void ShowScore()
+    {
+        Console.WriteLine($"=== {subjectName} ===");
+        Console.WriteLine($"점수: {score}/{maxScore}");
+        Console.WriteLine($"보너스 적용 횟수: {bounsCount}");
+    }
+}
+
--- ScoreTracker/Program.cs
+++ ScoreTracker/Program.cs
@@ -1,4 +1,14 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+ScoreTracker math = new ScoreTracker("수학");
+math.ShowScore();
+Console.WriteLine();
+
+math.SetScore(85);
+math.AddBonus(10);
+math.AddBonus(20);
+math.SetScore(-10);
+Console.WriteLine();
+
+math.ShowScore();
